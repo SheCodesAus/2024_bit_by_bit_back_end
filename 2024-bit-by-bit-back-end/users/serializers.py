050a5bbbc144c5ee6ca32062c2357d3f21a17980
COHALLY <CATHY.O'HALLORAN@RWWA.COM.AU>
2024-03-26 19:30:50 +0800

    Database test.

--- 2024-bit-by-bit-back-end/users/serializers.py
+++ 2024-bit-by-bit-back-end/users/serializers.py
@@ -8,10 +8,22 @@ class CustomUserSerializer(serializers.ModelSerializer):
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        return CustomUser.objects.create_user(**validated_data)
+        return CustomUser.objects.create(**validated_data)
     
 class UserProcessSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = UserProcess
         fields = '__all__'
+
+class CustomUserDetailSerializer(CustomUserSerializer):
+    # mentor = UserProcessSerializer(many=True, read_only=True)
+
+    def update(self, instance, validated_data):
+        instance.user_id  = validated_data.get('user_id', instance.user_id)
+        instance.user_onboarding_task = validated_data.get('user_onboarding_task', instance.user_onboarding_task)
+        instance.user_offboarding_task = validated_data.get('user_offboarding_task', instance.user_offboarding_task)
+        instance.is_completed = validated_data.get('is_completed', instance.is_completed)
+        instance.timestamp = validated_data.get('timestamp', instance.timestamp)
+        instance.save()
+        return instance
