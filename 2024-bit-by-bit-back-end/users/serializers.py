91801e1b1a3acb4d7924775d4a387eb2f348c3d7
COHally <147501090+COHally@users.noreply.github.com>
2024-04-05 10:16:43 +0800

    Updated user serializers.

--- 2024-bit-by-bit-back-end/users/serializers.py
+++ 2024-bit-by-bit-back-end/users/serializers.py
@@ -1,20 +1,21 @@
 from rest_framework import serializers
 from .models import CustomUser, UserProcess
 
+class UserProcessSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProcess
+        fields = '__all__'
+
 class CustomUserSerializer(serializers.ModelSerializer):
+    onboarded_mentor = UserProcessSerializer(many=True, read_only=True)
+
     class Meta:
         model = CustomUser
         fields = '__all__'
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        return CustomUser.objects.create(**validated_data)
-    
-class UserProcessSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = UserProcess
-        fields = '__all__'
+        return CustomUser.objects.create_user(**validated_data)
 
 class CustomUserDetailSerializer(CustomUserSerializer):
 
